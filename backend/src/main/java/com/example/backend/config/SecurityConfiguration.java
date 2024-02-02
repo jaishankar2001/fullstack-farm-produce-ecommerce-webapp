7bedf0547cfb270d850f6f77230ca969dbcadbb4
TANUJ VIPULKUMAR DOSHI <tn716378@dal.ca>
2024-02-02 13:10:03 -0400

    backend: Added: JWT and security configuration

--- backend/src/main/java/com/example/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfiguration.java
@@ -1,5 +1,44 @@
 package com.example.backend.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.backend.entities.Role;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfiguration {
-    
+    private final AuthenticationProvider authenticationProvider;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                    request -> {
+                        request.requestMatchers("/api/farmer/**").hasAuthority(Role.FARMER.name());
+                        request.requestMatchers("/api/customer/**").hasAuthority(Role.CUSTOMER.name());
+                        request.requestMatchers("/api/**").permitAll();
+                        request.anyRequest().authenticated();
+    })
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                
+
+        return http.build();
+
+    }
+
 }
