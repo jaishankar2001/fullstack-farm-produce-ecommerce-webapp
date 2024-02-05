4fed06743655623e951cc74db62a36919da5f12b
Nikita Davies <nk548914@dal.ca>
2024-02-05 19:11:03 -0400

    Frontend: Add register API integration #14

--- frontend/src/pages/SignUpPage/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import Layout from '../../common/Layout/Layout';
+import api from '../../api/index';
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    role: 'CUSTOMER'
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.auth.register({
+         email: formData.email, 
+         firstName: formData.firstName ,
+         lastName: formData.lastName,
+         password: formData.password,
+         role: formData.role
+        });
+
+        if(response){
+          navigate("/verify-email");
+        }
+    } catch (error) {
+      console.log(error);
+    }
+    
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="container py-3">
       <div className="row my-4">
@@ -9,29 +45,29 @@ function SignUp() {
           <div className="card border-0 shadow-sm">
             <div className="card-body px-4">
               <h4 className="card-title fw-bold mt-2 mb-4">Sign Up</h4>
-              <form className="row g-3">
+              <form className="row g-3" onSubmit={handleSubmit}>
                 <div className="col-md-6">
                   <label className="form-label">First Name</label>
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" name="firstName" onChange={handleChange} />
                 </div>
                 <div className="col-md-6">
                   <label className="form-label">Last Name</label>
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" name="lastName" onChange={handleChange} />
                 </div>
                 <div className="col-md-12">
                   <label className="form-label">Email</label>
-                  <input type="email" className="form-control" />
+                  <input type="email" className="form-control" name="email" onChange={handleChange} />
                 </div>
                 <div className="col-md-6">
                   <label className="form-label">Password</label>
-                  <input type="password" className="form-control" />
+                  <input type="password" className="form-control" name="password" onChange={handleChange} />
                 </div>
                 <div className="col-md-6">
                   <label className="form-label">Confirm Password</label>
                   <input type="password" className="form-control" />
                 </div>
                 <div className="col-md-12 mt-4">
-                  <button className="btn btn-primary w-100">Register</button>
+                  <button type="submit" className="btn btn-primary w-100">Register</button>
                 </div>
                 <div className="col-md-12">
                   <div className="text-muted bg-light rounded p-3 border small">
