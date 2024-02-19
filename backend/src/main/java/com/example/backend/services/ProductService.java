91d7c78e43fa7eeb6ef67b2db3ba3c32321c03ad
Nikita Davies <nk548914@dal.ca>
2024-02-19 19:10:38 -0400

    Backend: Add products list with search

--- backend/src/main/java/com/example/backend/services/ProductService.java
+++ backend/src/main/java/com/example/backend/services/ProductService.java
@@ -4,10 +4,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.security.Principal;
 import com.example.backend.dto.request.AddProductRequest;
+import com.example.backend.dto.request.ProductSearchRequest;
 import com.example.backend.dto.response.ProductDto;
 
 public interface ProductService {
     List<ProductDto> addProduct(AddProductRequest product, MultipartFile[] files, Principal principal);
 
     void deleteProduct(int id);
+
+    List<ProductDto> getFarmerProducts(Principal principal);
+
+    List<ProductDto> getAllProducts(ProductSearchRequest productSearchRequest);
 }
