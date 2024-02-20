28fa7600e03a93bd0a87081c8c419df130c9b06d
TANUJ VIPULKUMAR DOSHI <tn716378@dal.ca>
2024-02-20 15:04:19 -0400

    changed: allow origins

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://csci5308vm9.research.cs.dal.ca/")
+                        .allowedOrigins("*")
                         .allowedMethods(HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
                                 HttpMethod.DELETE.name())
