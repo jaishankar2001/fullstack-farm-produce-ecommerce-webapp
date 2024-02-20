4ba67d0b80cb923cef5e67b1ac0385b396ab7922
TANUJ VIPULKUMAR DOSHI <tn716378@dal.ca>
2024-02-20 14:32:04 -0400

    changes

--- frontend/src/api/utils.js
+++ frontend/src/api/utils.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_BASE_URL;
 
+console.log("IN UTLSSS", process.env.REACT_APP_BASE_URL);
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
 });
 
